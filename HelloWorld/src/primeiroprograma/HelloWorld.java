7350c86d8cdf966bca872beb33e717380d9300bb
thiagocalvi <thiagohcalvi@gmail.com>
2024-06-05 08:14:23 -0300

    mod prog hell world

--- HelloWorld/src/primeiroprograma/HelloWorld.java
+++ HelloWorld/src/primeiroprograma/HelloWorld.java
@@ -13,7 +13,7 @@ package primeiroprograma;
 public class HelloWorld{
     public static void main(String[] args){
         
-        System.out.println("Hello World");
+        System.out.println("Primeiro Hello World em Java");
     }
 
 }
