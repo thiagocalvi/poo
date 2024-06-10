21d4345f7137eb7d81993f30240a62e243986249
thiagocalvi <thiagohcalvi@gmail.com>
2024-06-10 08:23:13 -0300

    Aula #3

---
+++ LinguagemProgramacao/src/LeitorScanner/LeitorScanner.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package LeitorScanner;
+
+/**
+ *
+ * @author thiago
+ */
+import java.util.Scanner;
+
+public class LeitorScanner {
+    public static void main(String[] args){
+        Scanner leitura = new Scanner(System.in);
+    }
+}
